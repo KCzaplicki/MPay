f63ac3fe28ac5dbd179943cc8e54e13e483d9ee5
Krystian Czaplicki <krystianczaplicki@outlook.com>
2023-04-09 16:04:17 +0200

    Added swagger tags

--- api/src/MPay.API/Endpoints/HomeEndpoints.cs
+++ api/src/MPay.API/Endpoints/HomeEndpoints.cs
@@ -1,7 +1,15 @@
-﻿namespace MPay.API.Endpoints;
+﻿using System.Net;
+
+namespace MPay.API.Endpoints;
 
 internal static class HomeEndpoints
 {
+    private const string HomeTag = "Home";
+
     internal static void MapHomeEndpoints(this WebApplication app) =>
-        app.MapGet("/", () => "MPay API");
+        app.MapGet("/", () => "MPay API")
+            .WithTags(HomeTag)
+            .WithDescription("API footprint")
+            .Produces<string>()
+            .WithOpenApi();
 }
--- api/src/MPay.API/Endpoints/PurchaseEndpoints.cs
+++ api/src/MPay.API/Endpoints/PurchaseEndpoints.cs
@@ -1,38 +1,73 @@
-﻿using MPay.Infrastructure.DAL.UnitOfWork;
+﻿using System.Net;
+using System.Net.Mime;
+using MPay.Infrastructure.DAL.UnitOfWork;
 
 namespace MPay.API.Endpoints;
 
 internal static class PurchaseEndpoints
 {
     private const string BasePath = "/purchases";
+    
+    private const string PurchaseTag = "Purchase";
+    private const string PaymentTag = "Payment";
 
     internal static void MapPurchaseEndpoints(this WebApplication app)
     {
         var group = app.MapGroup(BasePath);
 
         group.MapPost("/", async (IPurchaseService purchaseService, AddPurchaseDto addPurchaseDto) =>
-        {
-            var id = await purchaseService.AddAsync(addPurchaseDto);
-            return TypedResults.Created($"{BasePath}/{id}", id);
-        })
-        .AddEndpointFilter<ValidationEndpointFilter>();
+            {
+                var id = await purchaseService.AddAsync(addPurchaseDto);
+                return TypedResults.Created($"{BasePath}/{id}", id);
+            })
+            .WithTags(PurchaseTag)
+            .WithDescription("Adds new purchase")
+            .Accepts<AddPurchaseDto>(MediaTypeNames.Application.Json)
+            .Produces<string>((int)HttpStatusCode.Created)
+            .Produces<ErrorDetails>((int)HttpStatusCode.BadRequest)
+            .Produces((int)HttpStatusCode.InternalServerError)
+            .WithOpenApi()
+            .AddEndpointFilter<ValidationEndpointFilter>();
 
-        group.MapGet("/{id}", async (IPurchaseService purchaseService, string id) 
-            => TypedResults.Ok(await purchaseService.GetPendingAsync(id)));
+        group.MapGet("/{id}", async (IPurchaseService purchaseService, string id)
+                => TypedResults.Ok(await purchaseService.GetPendingAsync(id)))
+            .WithTags(PurchaseTag)
+            .WithDescription("Gets pending purchase")
+            .Produces<PurchaseDto>()
+            .Produces<ErrorDetails>((int)HttpStatusCode.NotFound)
+            .Produces((int)HttpStatusCode.InternalServerError)
+            .WithOpenApi();
 
         group.MapPost("/{id}/cancel", async (IPurchaseService purchaseService, string id) =>
-        {
-            await purchaseService.CancelPurchaseAsync(id);
-            return TypedResults.NoContent();
-        });
+            {
+                await purchaseService.CancelPurchaseAsync(id);
+                return TypedResults.NoContent();
+            })
+            .WithTags(PurchaseTag)
+            .WithDescription("Cancels pending purchase")
+            .Produces((int)HttpStatusCode.NoContent)
+            .Produces<ErrorDetails>((int)HttpStatusCode.NotFound)
+            .Produces((int)HttpStatusCode.InternalServerError)
+            .WithOpenApi();
 
-        group.MapPost("/{id}/payment", 
-            async (IPurchasePaymentService purchasePaymentService, string id, PurchasePaymentDto purchasePaymentDto) =>
-        {
-            var result = await purchasePaymentService.ProcessPaymentAsync(id, purchasePaymentDto);
-            return result.IsCompleted ? (IResult)TypedResults.Ok() : TypedResults.BadRequest(ErrorDetailsExtensions.MapFrom(result));
-        })
-        .AddEndpointFilter<ValidationEndpointFilter>()
-        .AddEndpointFilter<TransactionalEndpointFilter>();
+        group.MapPost("/{id}/payment",
+                async (IPurchasePaymentService purchasePaymentService, string id,
+                    PurchasePaymentDto purchasePaymentDto) =>
+                {
+                    var result = await purchasePaymentService.ProcessPaymentAsync(id, purchasePaymentDto);
+                    return result.IsCompleted
+                        ? (IResult)TypedResults.Ok()
+                        : TypedResults.BadRequest(ErrorDetailsExtensions.MapFrom(result));
+                })
+            .WithTags(PaymentTag)
+            .WithDescription("Processes payment for pending purchase")
+            .Accepts<PurchasePaymentDto>(MediaTypeNames.Application.Json)
+            .Produces((int)HttpStatusCode.OK)
+            .Produces<ErrorDetails>((int)HttpStatusCode.BadRequest)
+            .Produces((int)HttpStatusCode.NotFound)
+            .Produces((int)HttpStatusCode.InternalServerError)
+            .WithOpenApi()
+            .AddEndpointFilter<ValidationEndpointFilter>()
+            .AddEndpointFilter<TransactionalEndpointFilter>();
     }
 }
